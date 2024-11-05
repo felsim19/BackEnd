fe02857d3e5ab71573554d45b1db2951509bc993
David Carrillo <david.carrillo.moncayo@gmail.com>
2024-11-05 14:44:10 -0500

    a

--- connection.py
+++ connection.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_URL = "mysql+mysqlconnector://root:0000@localhost:3306/fixflow"
+DB_URL = "mysql+mysqlconnector://root:2004@localhost:3306/fixflow"
 
 create = create_engine(DB_URL)
 
